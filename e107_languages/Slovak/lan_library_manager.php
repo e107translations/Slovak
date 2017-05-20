659e667c5729fa990ceca2f263b1df66c58eaf94
Jimmi08 <kika200598>
2017-05-20 19:15:39 +0200

    fixed lan_library_manager.php

--- e107_languages/Slovak/lan_library_manager.php
+++ e107_languages/Slovak/lan_library_manager.php
@@ -4,30 +4,30 @@
 |        e107 website content management system Slovak Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2017/03/31 07:16:56
+|        Last Modified: 2017/05/20 12:06:27
 |
-|        $Author: e107 $
+|        $Author: Jimako $
 +---------------------------------------------------------------+
 */
-define("LAN_LIBRARY_MANAGER_01", "Knižnica '[x]' , na ktorej knižnica '[y]' závisí, nie je nainštalovaná.");
-define("LAN_LIBRARY_MANAGER_02", "Verzia '[x]' knižnice '[y]' nie je kompatibilná s knižnicou '[z]'.");
-define("LAN_LIBRARY_MANAGER_03", "Nepodarilo sa nájsť knižnicu '[x]'.");
-define("LAN_LIBRARY_MANAGER_04", "Nepodarilo sa určiť verziu knižnice '[x]'.");
-define("LAN_LIBRARY_MANAGER_05", "Nainštalovaná verzia  '[x]' knižnice '[y]' nie je podporovaná.");
-define("LAN_LIBRARY_MANAGER_06", "Variant '[x]' knižnice '[y]' sa nenašiel.");
-define("LAN_LIBRARY_MANAGER_07", "chýbajúca dependency");
-define("LAN_LIBRARY_MANAGER_08", "nekompatibilná dependency");
+define("LAN_LIBRARY_MANAGER_01", "The [x] library, which the [y] library depends on, is not installed.");
+define("LAN_LIBRARY_MANAGER_02", "The version [x] of the [y] library is not compatible with the [z] library.");
+define("LAN_LIBRARY_MANAGER_03", "The [x] library could not be found.");
+define("LAN_LIBRARY_MANAGER_04", "The version of the [x] library could not be detected.");
+define("LAN_LIBRARY_MANAGER_05", "The installed version [x] of the [y] library is not supported.	");
+define("LAN_LIBRARY_MANAGER_06", "The [x] variant of the [y] library could not be found.");
+define("LAN_LIBRARY_MANAGER_07", "missing dependency");
+define("LAN_LIBRARY_MANAGER_08", "incompatible dependency");
 define("LAN_LIBRARY_MANAGER_10", "not detected");
 define("LAN_LIBRARY_MANAGER_11", "not supported");
-define("LAN_LIBRARY_MANAGER_13", "Knižnica");
-define("LAN_LIBRARY_MANAGER_21", "Provider");
+define("LAN_LIBRARY_MANAGER_13", "Library");
+define("LAN_LIBRARY_MANAGER_21", "	Provider");
 define("LAN_LIBRARY_MANAGER_25", "Third-party libraries");
 define("LAN_LIBRARY_MANAGER_27", "Machine name: [x]");
 define("LAN_LIBRARY_MANAGER_28", "Library path: [x]");
 define("LAN_LIBRARY_MANAGER_29", "Library path");
 define("LAN_LIBRARY_MANAGER_30", "CDN settings");
 define("LAN_LIBRARY_MANAGER_31", "Use CDN for core libraries");
-define("LAN_LIBRARY_MANAGER_32", "Use CDN for core libraries");
+define("LAN_LIBRARY_MANAGER_32", "CDN provider");
 
 
 ?>
