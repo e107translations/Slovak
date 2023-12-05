105e55403974781540b553f8d4cc46f17517eba5
Jimako <janamicek@gmail.com>
2023-12-05 12:29:49 +0100

    download plugin

---
+++ e107_plugins/download/languages/Slovak_global.php
@@ -0,0 +1,20 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Slovak Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2023/12/05 10:24:52 
+|            
+|        $URL: https://github.com/e107translations/Slovak/ $
+|        $Revision: 2.3.3 $
+|        $Author: Jimako $
++---------------------------------------------------------------+
+*/
+define("LAN_PLUGIN_DOWNLOAD_NAME", "Stiahnutia");
+define("LAN_PLUGIN_DOWNLOAD_DIZ", "Tento plugin je plne funkčný systém na sťahovanie súborov");
+define("LAN_DL_NT_01", "Hlásené chybné sťahovanie");
+define("LAN_DL_NT_02", "nasledujúce sťahovanie bolo nahlásené ako nefunkčné:");
+define("LAN_DL_NT_03", "Informoval o tom [x] s nasledujúcou poznámkou:");
+define("LAN_DL_NT_04", "Kliknutím na [tu] si môžete pozrieť hlásenia o nefunkčnom sťahovaní.");
+define("LAN_DL_LATEST_01", "Nahlásené nefunkčné stiahnutia");
---
+++ e107_plugins/download/languages/Slovak_log.php
@@ -0,0 +1,24 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Slovak Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2023/12/05 10:24:52 
+|            
+|        $URL: https://github.com/e107translations/Slovak/ $
+|        $Revision: 2.3.3 $
+|        $Author: Jimako $
++---------------------------------------------------------------+
+*/
+define("LAN_AL_DOWNL_01", "Stiahnutia - zmenené nastavenia sťahovania");
+define("LAN_AL_DOWNL_02", "Stiahnutia - zmenené nastavenia nahrávania");
+define("LAN_AL_DOWNL_03", "Stiahnutia - pridaný limit");
+define("LAN_AL_DOWNL_04", "Stiahnutia - log 04");
+define("LAN_AL_DOWNL_05", "Stiahnutia - log 05");
+define("LAN_AL_DOWNL_06", "Stiahnutia - log 06");
+define("LAN_AL_DOWNL_07", "Stiahnutia - log 07");
+define("LAN_AL_DOWNL_08", "Stiahnutia - log 08");
+define("LAN_AL_DOWNL_09", "Stiahnutia - log 09");
+define("LAN_AL_DOWNL_10", "Stiahnutia - aktualizovaný limit ");
+define("LAN_AL_DOWNL_11", "Stiahnutia - zrušený limit ");
