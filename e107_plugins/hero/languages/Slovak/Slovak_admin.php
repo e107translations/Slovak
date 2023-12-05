77357a080ae5e68fff5eb93d808b7f1d22dc6b51
Jimako <janamicek@gmail.com>
2023-12-05 12:39:21 +0100

    hero plugin

---
+++ e107_plugins/hero/languages/Slovak/Slovak_admin.php
@@ -0,0 +1,27 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Slovak Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2023/12/05 11:35:34
+|
+|        $Author: Jimako $
++---------------------------------------------------------------+
+*/
+
+define("LAN_HERO_ADMIN_001", "Defaultné slajdy boli importované.");
+define("LAN_HERO_ADMIN_002", "Nepodarilo sa naimportovať defaultné slajdy.");
+define("LAN_HERO_ADMIN_003", "Pozadie");
+define("LAN_HERO_ADMIN_004", "Obrázok/video");
+define("LAN_HERO_ADMIN_005", "Média");
+define("LAN_HERO_ADMIN_006", "Štítok");
+define("LAN_HERO_ADMIN_007", "Nezabudnite aktivovať hero menu  na domovskej stránke pomocou [Správca menu].");
+define("LAN_HERO_ADMIN_009", "Balíček s ikonami");
+define("LAN_HERO_ADMIN_010", "Štýl pre ikony");
+define("LAN_HERO_ADMIN_011", "Automatický interval slajdov");
+define("LAN_HERO_ADMIN_012", "Text");
+define("LAN_HERO_ADMIN_013", "Animácia");
+define("LAN_HERO_ADMIN_014", "Zdržanie (sec)");
+define("LAN_HERO_ADMIN_015", "Tlačítko");
+define("LAN_HERO_ADMIN_016", "[x] sekúnd");
---
+++ e107_plugins/hero/languages/Slovak/Slovak_global.php
@@ -0,0 +1,14 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Slovak Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2023/12/05 11:37:38
+|        $Author: Jimako $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_HERO_NAME", "Hero");
+define("LAN_PLUGIN_HERO_SUMMARY", "Správa Hero na domovskej stránke");
+define("LAN_PLUGIN_HERO_DESCRIPTION", "Obrázkový a textový slider s animovanými odrážkami pre Hero vašej úvodnej stránky.");
